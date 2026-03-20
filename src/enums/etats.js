3b209aa2065b6559472e5e63d55888eee3827213
TCG-Blob <oriane.mejean@etu.univ-amu.fr>
2026-03-20 16:22:06 +0100

    ajout des données donner du sujet

---
+++ src/enums/etats.js
@@ -0,0 +1,13 @@
+export const ETATS = {
+    NOUVEAU: 'Nouveau',
+    EN_COURS: 'En cours',
+    REUSSI: 'Réussi',
+    EN_ATTENTE: 'En attente',
+    ABANDONNE: 'Abandonné',
+}
+
+export const ETAT_TERMINE = [
+    ETATS.REUSSI,
+    ETATS.ABANDONNE,
+]
+
