02010d1a73266af4a47054b6c82b119c796e926f
TCG-Blob <oriane.mejean@etu.univ-amu.fr>
2026-03-21 13:20:23 +0100

    adaptation de la convention de nommage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import App from './components/app/App';
 import { TodoProvider } from './context/TodoContext';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
