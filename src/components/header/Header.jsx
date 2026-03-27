96444dd88e6fe092ca58c0b6625f884570e5e3f7
TCG-Blob <oriane.mejean@etu.univ-amu.fr>
2026-03-27 10:13:59 +0100

    ajout des taches total et des taches terminés

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,31 @@
 import logo from "../../logo.svg";
-import "./Header.css"
+import { useTodo } from "../../context/TodoContext";
+import { ETAT_TERMINE } from "../../enums/Etats";
+import "./Header.css";
 
 const Header = () => {
+    const { taches } = useTodo();
+    const nbTotal = taches.length;
+    const nbNonTerminees = taches.filter((t) => !ETAT_TERMINE.includes(t.etat)).length;
     const refreshPage = () => {
         window.location.reload();
     };
 
-
     return (
         <header className="header">
             <img src={logo} alt="logo" onClick={refreshPage} />
-            <h1>ToDo-List</h1>
+            <h1 className="headerTitre">ToDo-List</h1>
+            <div className="headerStats">
+                <div className="headerStat">
+                    <span className="headerStatNombre">{nbTotal}</span>
+                    <span className="headerStatLabel">tâches au total</span>
+                </div>
+                <div className="headerSeparateur" />
+                <div className="headerStat">
+                    <span className="headerStatNombre headerStatNombreEncours">{nbNonTerminees}</span>
+                    <span className="headerStatLabel">non terminées</span>
+                </div>
+            </div>
         </header>
     );
 };
